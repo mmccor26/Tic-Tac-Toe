6661f478e6736c1c9d9612725a0f50e7b305ca2d
mmccor26 <43655058+mmccor26@users.noreply.github.com>
2019-06-16 11:00:53 -0400

    Check for Win  Function Logic Added

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -1,9 +1,9 @@
 var playerTurn = 0;
 const GameBoard = () => {
     board = [
-        [null,null,null],
-        [null,null,null],
-        [null,null,null]
+        ["","",""],
+        ["","",""],
+        ["","",""]
     ]
 
 };
@@ -25,20 +25,44 @@ function render(){
     }
 
 }
-
+function updateArray(){
+    cell1 = document.getElementById("cell1");
+    cell2 = document.getElementById("cell2");
+    cell3 = document.getElementById("cell3");
+    cell4 = document.getElementById("cell4");
+    cell5 = document.getElementById("cell5");
+    cell6 = document.getElementById("cell6");
+    cell7 = document.getElementById("cell7");
+    cell8 = document.getElementById("cell8");
+    cell9 = document.getElementById("cell9");
+    board[0][0] = cell1.innerHTML;
+    board[0][1] = cell1.innerHTML;
+    board[0][2] = cell1.innerHTML;
+    board[1][0] = cell1.innerHTML;
+    board[1][1] = cell1.innerHTML;
+    board[1][2] = cell1.innerHTML;
+    board[2][0] = cell1.innerHTML;
+    board[2][1] = cell1.innerHTML;
+    board[2][2] = cell1.innerHTML;
+    
+}
 tableCells = document.querySelectorAll("td");
 tableCells.forEach((cell) => {
+    
     cell.addEventListener('click',(e) => {
-        if(playerTurn == 0){
+        if(playerTurn == 0 && cell.innerHTML == " "){
             console.log("X")
             cell.innerHTML = "X";
             playerTurn = 1;
         }
-        else{
+        else if(playerTurn == 1 && cell.innerHTML == " "){
             console.log("O");
             cell.innerHTML = "O";
             playerTurn = 0;
         }
+        
+        updateArray();
+        console.log(board);
         
         
         render();
@@ -46,6 +70,40 @@ tableCells.forEach((cell) => {
 });
 
 function checkWin(array){
+    //123
+    if(array[0][0] == array[0][1]==array[0][2]){
+
+    }
+    //456
+    if(array[1][0] == array[1][1]==array[1][2]){
+        
+    }
+    //789
+    if(array[2][0] == array[2][1]==array[2][2]){
+        
+    }
+    //147
+    if(array[0][0] == array[1][0]==array[2][0]){
+        
+    }
+    //258
+    if(array[0][1] == array[1][1]==array[2][1]){
+        
+    }
+    //369
+    if(array[0][2] == array[1][2]==array[2][2]){
+        
+    }
+    //diagonals
+    if(array[0][0] == array[1][1]==array[2][2]){
+        
+    }
+    if(array[0][2] == array[1][1]==array[2][0]){
+        
+    }
+
+
+    
     
 }
 
