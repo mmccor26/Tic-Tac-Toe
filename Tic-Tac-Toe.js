b93eb7f5a1c6ca12e47b42236f48abeec064c7ec
mmccor26 <43655058+mmccor26@users.noreply.github.com>
2019-06-16 09:53:16 -0400

    Player Choice Display Added

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -1,3 +1,4 @@
+var playerTurn = 0;
 const GameBoard = () => {
     board = [
         [null,null,null],
@@ -11,10 +12,43 @@ const GameController = () => {
 
 };
 
-const Player = (() => {
-
-})();
+const Player = (num) => {
+    return num;
+};
 
 function render(){
+
+    for(i = 0; i<=3;i++){
+        for(i = 0; i<=3;i++){
+
+        }  
+    }
+
+}
+
+tableCells = document.querySelectorAll("td");
+tableCells.forEach((cell) => {
+    cell.addEventListener('click',(e) => {
+        if(playerTurn == 0){
+            console.log("X")
+            cell.innerHTML = "X";
+            playerTurn = 1;
+        }
+        else{
+            console.log("O");
+            cell.innerHTML = "O";
+            playerTurn = 0;
+        }
+        
+        
+        render();
+    });
+});
+
+function checkWin(array){
     
 }
+
+const player1 = Player(1);
+const player2 = Player(2);
+
